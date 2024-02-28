d8c51856745940b7c271953f64c0f6f1a41c06b6
Oleksandr Nemesh <prevter@gmail.com>
2024-02-28 21:03:03 +0200

    add GJGameState members

--- include/dash/bindings/GJGameState.hpp
+++ include/dash/bindings/GJGameState.hpp
@@ -6,5 +6,10 @@ namespace gd {
     class GJGameState {
     public:
         SETUP_MEMBER(float, m_stepSpeed, "GJGameState::m_stepSpeed")
+        SETUP_MEMBER(cocos2d::CCPoint, m_cameraPosition, "GJGameState::m_cameraPosition")
+        SETUP_MEMBER(float, m_cameraAngle, "GJGameState::m_cameraAngle")
+        SETUP_MEMBER(float, m_targetCameraAngle, "GJGameState::m_targetCameraAngle")
+        SETUP_MEMBER(float, m_cameraScale, "GJGameState::m_cameraScale")
+        SETUP_MEMBER(float, m_targetCameraScale, "GJGameState::m_targetCameraScale")
     };
 }
