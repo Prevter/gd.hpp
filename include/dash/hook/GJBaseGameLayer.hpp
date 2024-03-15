c12254b6e5bba333a5819c9224bc01f1c511ceb7
Oleksandr Nemesh <prevter@gmail.com>
2024-03-15 02:04:40 +0200

    add GJBaseGameLayer::update hook

--- include/dash/hook/GJBaseGameLayer.hpp
+++ include/dash/hook/GJBaseGameLayer.hpp
@@ -21,4 +21,20 @@ namespace gd::hook::GJBaseGameLayer {
     /// @brief Installs the hook for GameStatsManager::isItemUnlocked.
     SETUP_HOOK("GJBaseGameLayer::processCommands", void, processCommands, gd::GJBaseGameLayer*)
 
+    // GJBaseGameLayer::update
+
+    inline static uintptr_t update_addr = 0x0;
+    inline static std::function<void(gd::GJBaseGameLayer*, float)> update_hook;
+
+    /// @brief Calls the original GJBaseGameLayer::update function.
+    inline static void update(gd::GJBaseGameLayer* self, float dt) {
+        reinterpret_cast<void (__thiscall *)(gd::GJBaseGameLayer*, float)>(update_addr)(self, dt);
+    }
+
+    /// @brief Wrapper for the GJBaseGameLayer::update hook.
+    inline void __fastcall updateHook(gd::GJBaseGameLayer* self, float dt) { update_hook(self, dt); }
+
+    /// @brief Installs the hook for GJBaseGameLayer::update.
+    SETUP_HOOK("GJBaseGameLayer::update", void, update, gd::GJBaseGameLayer*, float)
+
 }
